0fe9d33531944df4d401f7c747a77deb23e3a3f0
LombameLemma <Lombamelemma@gmail.com>
2025-09-13 09:45:41 -0700

    Update PalindromeNumber.py

--- PalindromeNumber.py
+++ PalindromeNumber.py
@@ -1,1 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        # Negative numbers are not palindromes.
+        if x < 0:
+            return False
+
+        # Convert the integer to a string.
+        s = str(x)
+
+        # Reverse the string and compare it to the original.
+        return s == s[::-1]
 
