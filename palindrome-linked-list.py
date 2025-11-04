8a9be2ada81e5afc4d54ea7c008faf25f1162578
LombameLemma <Lombamelemma@gmail.com>
2025-11-04 14:01:07 +0300

    Create palindrome-linked-list.py

---
+++ palindrome-linked-list.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        current=head
+        val=[]
+        while current:
+            val.append(current.val)
+            current=current.next
+        return val==val[::-1]
